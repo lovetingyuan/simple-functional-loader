5b500622bcc93bd04659940235416d5770904c39
Baitu Huang <baitu.huang@shopeemobile.com>
2018-11-27 15:08:54 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # simple-functional-loader
 use function as webpak loader option
+
+## install
+```bash
+npm install simple-functional-loader --save-dev
+```
+
+## usage
+```
+// webpack.config.js
+const { createLoader } = require('simple-functional-loader')
+module.exports = {
+  //...
+  module: {
+    rules: [
+      {
+        test: /.html$/,
+        use: createLoader(function(source) { // must be an es5 function, can not be arrow function or class.
+          return processSource(source)
+        })
+      }
+    ]
+  }
+}
+```
+
+## license
+MIT
---
+++ index.js
@@ -0,0 +1,24 @@
+const loadersMap = Object.create(null);
+const Err = msg => new Error('simple-functional-loader: ' + msg);
+
+module.exports = function simpleFunctionalLoader(...args) {
+  const id = this.query.__loadercallbackid;
+  return loadersMap[id].call(this, ...args);
+};
+
+module.exports.createLoader = function createLoader(processor) {
+  if (typeof processor !== 'function') {
+    throw Err('argument passed to createLoader must be a (es5) function.\n' + processor);
+  }
+  if (!processor.toString().startsWith('function')) {
+    throw Err('argument passed to createLoader can not be arrow function or class.\n' + processor);
+  }
+  const id = Math.random();
+  loadersMap[id] = processor;
+  return {
+    loader: __filename,
+    options: {
+      __loadercallbackid: id
+    }
+  };
+};
