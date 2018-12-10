2d360698d99a84cd2feb186df4484da6c9afdbf7
Baitu Huang <baitu.huang@shopeemobile.com>
2018-12-10 10:19:10 +0800

    test: add test

--- test/index.js
+++ test/index.js
@@ -3,10 +3,12 @@ const webpack = require('webpack');
 const memoryfs = require('memory-fs');
 const { createLoader } = require('../index');
 const fs = require('fs');
-const resolve = p => require('path').resolve(__dirname, p);
+const path = require('path');
+const resolve = p => path.resolve(__dirname, p);
 const test = require('tape');
 
 test('simple functional loader', function (t) {
+  const mark = Date.now() + '_' + Math.random();
   const compiler = webpack({
     entry: resolve('main.js'),
     module: {
@@ -14,7 +16,7 @@ test('simple functional loader', function (t) {
         test: /\.html$/,
         use: createLoader(function(source) {
           t.equal(source, fs.readFileSync(resolve('test.html'), 'utf8'));
-          return `module.exports = ${JSON.stringify(source.trim())}`;
+          return `module.exports = ${JSON.stringify(source.trim() + `<!--${mark}-->`)}`;
         })
       }, {
         test: /.ts$/,
@@ -30,6 +32,10 @@ test('simple functional loader', function (t) {
     if (err || stats.hasErrors()) {
       t.fail(err || 'webpack has errors.');
     } else {
+      const fs = compiler.outputFileSystem;
+      const result = fs.readFileSync(path.join(compiler.outputPath, 'main.js'), 'utf8');
+      t.equal(result.indexOf(mark) > 0, true);
+      t.equal(result.indexOf('functional loader') > 0, true);
       t.end();
     }
   });
