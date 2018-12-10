a1676a4782d0ca574cffc700caff7fdafcfbf89b
Baitu Huang <baitu.huang@shopeemobile.com>
2018-12-10 10:32:36 +0800

    test: add case

--- test/index.js
+++ test/index.js
@@ -8,7 +8,8 @@ const resolve = p => path.resolve(__dirname, p);
 const test = require('tape');
 
 test('simple functional loader', function (t) {
-  const mark = Date.now() + '_' + Math.random();
+  const markHTML = Date.now() + '_' + Math.random();
+  const markTs = Date.now() + '_' + Math.random();
   const compiler = webpack({
     entry: resolve('main.js'),
     module: {
@@ -19,17 +20,23 @@ test('simple functional loader', function (t) {
           t.equal('resourceQuery' in this, true);
           t.equal(this.webpack, true);
           t.equal(this.loaderIndex, 0);
-          return `module.exports = ${JSON.stringify(source.trim() + `<!--${mark}-->`)}`;
+          return `module.exports = ${JSON.stringify(source.trim() + `<!--${markHTML}-->`)}`;
         })
       }, {
         test: /.ts$/,
-        use: createLoader(function(source) {
-          t.equal(source, fs.readFileSync(resolve('test.ts'), 'utf8'));
+        use: [createLoader(function(source) {
+          t.equal(source.indexOf(markTs) > 0, true);
           t.equal('resourcePath' in this, true);
           t.equal(this.webpack, true);
           t.equal(this.loaderIndex, 0);
-          return source.replace(/:\s*\w+? /g, ' ');
-        })
+          return source;
+        }), createLoader(function(source) {
+          t.equal(source, fs.readFileSync(resolve('test.ts'), 'utf8'));
+          t.equal('resource' in this, true);
+          t.equal(this.webpack, true);
+          t.equal(this.loaderIndex, 1);
+          return source.replace(/:\s*\w+? /g, ' ') + `/*${markTs}*/`;
+        })]
       }]
     }
   });
@@ -40,7 +47,7 @@ test('simple functional loader', function (t) {
     } else {
       const fs = compiler.outputFileSystem;
       const result = fs.readFileSync(path.join(compiler.outputPath, 'main.js'), 'utf8');
-      t.equal(result.indexOf(mark) > 0, true);
+      t.equal(result.indexOf(markHTML) > 0, true);
       t.equal(result.indexOf('functional loader') > 0, true);
       t.end();
     }
