1bfeaa4a68e729b1658bc0f6f290d39136715576
tingyuan <baitu.huang@shopeemobile.com>
2019-05-31 11:12:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const loaderUtils = require('loader-utils')
-const loaderFuncsMap = Object.create(null);
+const loaderFuncsMap = Object.create(null)
 
 module.exports = function simpleFunctionalLoader(...args) {
   const { id } = loaderUtils.getOptions(this)
-  return loaderFuncsMap[id].call(this, ...args);
-};
+  return loaderFuncsMap[id].call(this, ...args)
+}
 
 module.exports.createLoader = function createLoader(processor) {
   if (
     typeof processor !== 'function' ||
     Function.prototype.toString.call(processor).indexOf('function')
   ) {
-    throw new Error('simple-functional-loader: parameter passed to "createLoader" must be an ES5 function.\n' + processor);
+    throw new Error('simple-functional-loader: parameter passed to "createLoader" must be an ES5 function.\n' + processor)
   }
-  const id = Date.now() + '_' + Math.random();
-  loaderFuncsMap[id] = processor;
+  const id = Date.now() + '_' + Math.random()
+  loaderFuncsMap[id] = processor
   return {
     loader: __filename,
     options: { id },
     ident: 'simple-functional-loader-' + id
-  };
-};
+  }
+}
