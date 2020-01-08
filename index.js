d89bc65fb2a6e3525639aeb13eec6ca8cd4dc3ad
baitu.huang <baitu.huang@shopee.com>
2020-01-08 18:51:27 +0800

    remove id

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const loaderUtils = require('loader-utils')
-const loaderFuncsMap = Object.create(null)
+const name = require('./package.json').name
 
 module.exports = function simpleFunctionalLoader(...args) {
-  const { id } = loaderUtils.getOptions(this)
-  return loaderFuncsMap[id].call(this, ...args)
+  const { processor } = loaderUtils.getOptions(this)
+  return processor.call(this, ...args)
 }
 
 module.exports.createLoader = function createLoader(processor) {
@@ -11,13 +11,11 @@ module.exports.createLoader = function createLoader(processor) {
     typeof processor !== 'function' ||
     Function.prototype.toString.call(processor).indexOf('function')
   ) {
-    throw new Error('simple-functional-loader: parameter passed to "createLoader" must be an ES5 function.\n' + processor)
+    throw new Error(name + ': parameter passed to "createLoader" must be an ES5 function.\n' + processor)
   }
-  const id = Date.now() + '_' + Math.random()
-  loaderFuncsMap[id] = processor
   return {
     loader: __filename,
-    options: { id },
-    ident: 'simple-functional-loader-' + id
+    options: { processor },
+    ident: name + '-' + Math.random()
   }
 }
